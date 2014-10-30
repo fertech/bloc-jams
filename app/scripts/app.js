ca53799a68459c9bf13d5236580d6219c7ebb2dc
Fernanda Aispuro <fernanda@weddingful.com>
2014-10-29 17:45:47 -0700

    Converted album view to Angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,21 +18,29 @@
 blocJams = angular.module('BlocJams', ['ui.router']);
 
 blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
-   $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
  
-   $stateProvider.state('landing', {
-     url: '/',
-     controller: 'Landing.controller',
-     templateUrl: '/templates/landing.html'
-   });
+  $stateProvider.state('landing', {
+    url: '/',
+    controller: 'Landing.controller',
+    templateUrl: '/templates/landing.html'
+  });
 
-   $stateProvider.state('collection', {
-     url: '/collection',
-     controller: 'Collection.controller',
-     templateUrl: '/templates/collection.html'
-   });
+  $stateProvider.state('collection', {
+    url: '/collection',
+    controller: 'Collection.controller',
+    templateUrl: '/templates/collection.html'
+  });
 
-// example for homewor. Not sure if keeping.
+
+  $stateProvider.state('album', {
+    url: '/album',
+    templateUrl: '/templates/album.html',
+    controller: 'Album.controller'
+  });
+
+
+// example for homework. Not sure if keeping.
    $stateProvider.state('song', {
         url: '/song',
         templateUrl: '/templates/song.html'
@@ -72,4 +80,37 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
     for (var i = 0; i < 33; i++) {
      $scope.albums.push(angular.copy(albumPicasso));
    }
+  }]);
+
+  blocJams.controller('Album.controller', ['$scope', function($scope) {
+    $scope.album = angular.copy(albumPicasso);
+
+    var hoveredSong = null;
+    var playingSong = null;
+ 
+    $scope.onHoverSong = function(song) {
+      hoveredSong = song;
+    };
+ 
+    $scope.offHoverSong = function(song) {
+      hoveredSong = null;
+    };
+
+    $scope.getSongState = function(song) {
+     if (song === playingSong) {
+       return 'playing';
+     }
+     else if (song === hoveredSong) {
+       return 'hovered';
+     }
+     return 'default';
+   };
+   
+    $scope.playSong = function(song) {
+      playingSong = song;
+    };
+ 
+    $scope.pauseSong = function(song) {
+      playingSong = null;
+    };
   }]);
