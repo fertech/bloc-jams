5494f057b186247ab3bac7f4f918f4aa4d7b3c2a
Fernanda Aispuro <fernanda@weddingful.com>
2014-11-26 22:13:24 -0800

    Changed slider to update data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,11 +7,11 @@
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-        { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-        { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-        { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-        { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-        { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
      ]
  };
  
@@ -66,6 +66,13 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
       pause: function() {
         this.playing = false;
         currentSoundFile.pause();
+      },
+      seek: function(time) {
+       // Checks to make sure that a sound file is playing before seeking.
+       if(currentSoundFile) {
+         // Uses a Buzz method to set the time of the song.
+         currentSoundFile.setTime(time);
+       }
       },
       setSong: function(album, song) {
         if (currentSoundFile) {
@@ -182,19 +189,47 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
      offsetXPercent = Math.min(1, offsetXPercent);
      return offsetXPercent;
    }
+
+   var numberFromValue = function(value, defaultValue) {
+     if (typeof value === 'number') {
+       return value;
+     }
+ 
+     if(typeof value === 'undefined') {
+       return defaultValue;
+     }
+ 
+     if(typeof value === 'string') {
+       return Number(value);
+     }
+   }
+ 
     return {
       templateUrl: '/templates/directives/slider.html',
       replace: true,
       restrict: 'E',
-      scope: {},
+      scope: {
+         onChange: '&'
+      },
       link: function(scope, element, attributes) {
         scope.value = 0;
-        scope.max = 200;
+        scope.max = 100;
+
+        console.log(attributes);
+        attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+ 
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
 
         var $seekBar = $(element);
 
         var percentString = function () {
-          percent = Number(scope.value) / Number(scope.max)  * 100;
+          var value = scope.value || 0;
+          var max = scope.max || 100;
+          percent = value / max * 100;
           return percent + "%";
         }
  
@@ -209,6 +244,7 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
         scope.onClickSlider = function(event) {
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.value = percent * scope.max;
+         notifyCallback(scope.value);
        }
 
        scope.trackThumb = function() {
@@ -216,6 +252,7 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
            var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
            scope.$apply(function(){
              scope.value = percent * scope.max;
+             notifyCallback(scope.value);
            });
          });
  
@@ -224,6 +261,11 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
            $document.unbind('mousemove.thumb');
            $document.unbind('mouseup.thumb');
          });
+       };
+       var notifyCallback = function(newValue) {
+         if(typeof scope.onChange === 'function') {
+           scope.onChange({value: newValue});
+         }
        };
       }
     };
